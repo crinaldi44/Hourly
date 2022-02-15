bcb628c70d4ff89856e798a71ac9d0891b499c16
crinaldi44 <91025658+crinaldi44@users.noreply.github.com>
2022-02-15 06:55:14 -0500

    Update database.py

--- models/database.py
+++ models/database.py
@@ -7,6 +7,6 @@ from flask import current_app
 # the start of a web request, call Session.commit(), and then
 # close the session at the end of the request. The general
 # preferred practice is to use the 'with' keyword.
-engine = create_engine('mysql+pymysql://crinaldi:7539mmr@localhost:3306/hourly', echo=True)
+engine = create_engine('mysql+pymysql://root:test123@localhost:3306/employees', echo=True)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
